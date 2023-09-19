2596012ed719e2ce9703e8ac71defacbeda56cf6
Mingi Kim <migni4575@gmail.com>
2023-09-19 10:19:52 +0900

    Initialize Spring Project

---
+++ src/main/java/io/migni/github/submodule/project/GithubSubmoduleExerciseProjectApplication.java
@@ -0,0 +1,13 @@
+package io.migni.github.submodule.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSubmoduleExerciseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubSubmoduleExerciseProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/migni/github/submodule/project/GithubSubmoduleExerciseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.migni.github.submodule.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSubmoduleExerciseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
